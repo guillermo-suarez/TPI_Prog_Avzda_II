1261b2b839cf8cdb7e955224b8d59d5f5f63fa82
Guillermo Suarez <gguillermosuarezz@gmail.com>
2021-10-08 20:29:46 -0300

    Se agregaron algunas cuestiones de vistas

--- TPI_Prog_Avzda_II/src/App.java
+++ TPI_Prog_Avzda_II/src/App.java
@@ -1,5 +1,12 @@
+import javax.swing.JFrame;
+
+import Vistas.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        CrearCultivo ventana = new CrearCultivo();
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
