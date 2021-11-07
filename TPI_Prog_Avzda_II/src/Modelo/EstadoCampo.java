8e1faaa248c1b6f85d39d8d56088a39fdf424c55
Guillermo Suarez <40548346+guillermo-suarez@users.noreply.github.com>
2021-11-06 23:27:42 -0300

    Delete TPI_Prog_Avzda_II directory

--- TPI_Prog_Avzda_II/src/Modelo/EstadoCampo.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public class EstadoCampo extends Estado
-{
-    public EstadoCampo(Integer numeroEstado, String descripcion)
-    {
-        super(numeroEstado, descripcion);
-    }
-}
--- TPI_Prog_Avzda_II/src/Modelo/EstadoProyecto.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public class EstadoProyecto extends Estado
-{
-    public EstadoProyecto(Integer numeroEstado, String descripcion)
-    {
-        super(numeroEstado, descripcion);
-    }
-}
