2e8279fdc033b477038bd82e647ca89ae9f52585
Guillermo Suarez <40548346+guillermo-suarez@users.noreply.github.com>
2021-11-10 18:42:47 -0300

    Add files via upload

--- Controlador/Controlador.java
+++ Controlador/Controlador.java
@@ -7,10 +7,16 @@ import java.util.*;
 public class Controlador {
     private DAOPostgres DAO;
     private List<Estadocampo> estadosCampo = new ArrayList<>();
+    private List<Estadoproyecto> estadosProyecto = new ArrayList<>();
+    private List<Laboreo> laboreos = new ArrayList<>();
+    private List<Cultivo> cultivos = new ArrayList<>();
     
     public Controlador(DAOPostgres DAO) {
         this.DAO = DAO;
         this.estadosCampo = DAO.recuperarTodos(Estadocampo.class);
+        this.laboreos = DAO.recuperarTodos(Laboreo.class);
+        this.cultivos = DAO.recuperarTodos(Cultivo.class);
+        
     }
     
     public void agregarObjeto(Object objeto) {
@@ -28,5 +34,20 @@ public class Controlador {
     public List<Estadocampo> getEstadosCampo() {
         this.estadosCampo = DAO.recuperarTodos(Estadocampo.class);
         return estadosCampo;
+    }
+    
+    public List<Estadoproyecto> getEstadosProyecto() {
+        this.estadosProyecto = DAO.recuperarTodos(Estadoproyecto.class);
+        return estadosProyecto;
+    }
+    
+    public List<Laboreo> getLaboreos() {
+        this.laboreos = DAO.recuperarTodos(Laboreo.class);
+        return laboreos;
+    }
+    
+    public List<Cultivo> getCultivos() {
+        this.cultivos = DAO.recuperarTodos(Cultivo.class);
+        return cultivos;
     }
 }
