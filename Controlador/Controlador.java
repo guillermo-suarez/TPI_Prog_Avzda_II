3f8b311ed22987e2b6bc9356f3801328d808e810
Guillermo Suarez <40548346+guillermo-suarez@users.noreply.github.com>
2021-11-13 19:34:04 -0300

    Add files via upload

--- Controlador/Controlador.java
+++ Controlador/Controlador.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class Controlador {
     private DAOPostgres DAO;
+    private List<Campo> campos = new ArrayList<>();
+    private List<Tiposuelo> tiposSuelo = new ArrayList<>();
     private List<Estadocampo> estadosCampo = new ArrayList<>();
     private List<Estadoproyecto> estadosProyecto = new ArrayList<>();
     private List<Laboreo> laboreos = new ArrayList<>();
@@ -13,10 +15,12 @@ public class Controlador {
     
     public Controlador(DAOPostgres DAO) {
         this.DAO = DAO;
+        this.campos = DAO.recuperarTodos(Campo.class);
+        this.tiposSuelo = DAO.recuperarTodos(Tiposuelo.class);
         this.estadosCampo = DAO.recuperarTodos(Estadocampo.class);
+        this.estadosProyecto = DAO.recuperarTodos(Estadoproyecto.class);
         this.laboreos = DAO.recuperarTodos(Laboreo.class);
         this.cultivos = DAO.recuperarTodos(Cultivo.class);
-        
     }
     
     public void agregarObjeto(Object objeto) {
@@ -29,6 +33,16 @@ public class Controlador {
     
     public void actualizarObjeto(Object objeto) {
         this.DAO.actualizarObjeto(objeto);
+    }
+    
+    public List<Campo> getCampos() {
+        this.campos = DAO.recuperarTodos(Campo.class);
+        return campos;
+    }
+    
+    public List<Tiposuelo> getTiposSuelo() {
+        this.tiposSuelo = DAO.recuperarTodos(Tiposuelo.class);
+        return tiposSuelo;
     }
 
     public List<Estadocampo> getEstadosCampo() {
